4276386db4d2653a57b8f27758d77b2d3d5919bb
Dustu6009 <rupamdasdustu1947@gmail.com>
2020-08-10 17:42:38 +0530

    First pusing project

---
+++ core java/src/com/rupam/overload/A.java
@@ -0,0 +1,23 @@
+package com.rupam.overload;
+
+class Main
+{
+	
+      public void sum(int x,int y)
+      {
+    	  System.out.println(x+y);
+      }
+      
+    
+}    
+	
+    public class A 
+    {
+	public static void main(String[] args)
+	{
+		
+       Main m=new Main();
+       m.sum(10,20);
+	}
+
+}
